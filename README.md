ce9585983313dc84c790714a9a284aa74f25fb88
Josiah Sprague <josiah.sprague@gmail.com>
2016-07-26 14:23:23 +0200

    update retext plugins and rules

--- README.md
+++ README.md
@@ -1,17 +1,30 @@
 # quality-docs
-A CLI tool to evaluate the quality of writing in your project's documentation.
+
+A CLI tool to check the quality of writing in your project's documentation.
 
 ## Installation
+
 Install the tool globally with npm to use the CLI.
-```
+
+```bash
 npm install -g quality-docs
 ```
 
 ## Usage
-The CLI accepts one argument which is a glob of the documentation files you want to check. For example, to recursively check all `.md` files in your project;
-```
+
+The CLI accepts one argument which is a glob of the documentation files you want to check. For example, to recursively check every `.md` file in your project;
+
+```bash
 quality-docs {,**/}*.md
 ```
 
+The tool prints a report of writing quality issues with their location and description. For example, an issue in README.md from line 23 column 9 to line 23 column 16 prints;
+
+```bash
+README.md
+  23:9-23:16   warning  Replace “utilize” with “use”
+```
+
 ## Other Notes
-This tool uses [retext](https://github.com/wooorm/retext) along with selected plugins to evaluate the quality of writing in your project's documentation.
+
+This tool uses [retext](https://github.com/wooorm/retext) to check the quality of writing in your project's documentation.
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 var argv = require('minimist')(process.argv.slice(2));
+var concise = require('retext-intensify');
+var equality = require('retext-equality');
 var fs = require('fs');
+var lint = require('remark-lint');
 var map = require("async/map");
-var remark = require('remark');
-var retext = require('retext');
 var readability = require('retext-readability');
+var remark = require('remark');
 var remark2retext = require('remark-retext');
 var report = require('vfile-reporter');
+var retext = require('retext');
+var simplify = require('retext-simplify');
 
 var docFiles = argv._;
 
@@ -21,7 +25,13 @@ map(docFiles, getFiles, function(err, files){
   var hasErrors = false;
   files.forEach((file) => {
     remark()
-      .use(remark2retext, retext().use(readability))
+      .use(lint, {'maximum-line-length': false})
+      .use(remark2retext, retext() // Convert markdown to plain text
+        .use(readability, {age: 18}) // Target age is low so that understanding requires less effort
+        .use(simplify) // Check for unneccesary complexity
+        .use(equality) // Check for inconsiderate language
+        .use(concise) // Check for filler words to make writing more concise
+      )
       .process(file.contents, function (err, results) {
         results.filename = file.name;
         allResults.push(results);
